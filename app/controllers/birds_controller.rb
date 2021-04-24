0bc11420a4ce61ad63c46b0b35a93bc576de0fa0
Laura Messa Travin <laura.travin@gmail.com>
2021-04-24 19:19:54 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -9,7 +9,11 @@ class BirdsController < ApplicationController
 #   render json: bird
 #   render json: bird.slice(:id, :name, :species)
     render json: bird, only: [:id, :name, :species]
-
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
  end
 
 end
