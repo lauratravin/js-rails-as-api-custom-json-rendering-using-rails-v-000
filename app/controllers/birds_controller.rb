c54d8117dbd60023c1b86c7f5ea474fd14254d4f
Laura Messa Travin <laura.travin@gmail.com>
2021-04-24 18:59:48 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,7 +1,8 @@
 class BirdsController < ApplicationController
   def index
     birds = Bird.all
-    render json: birds, except: [:created_at, :updated_at] # to filter
+#    render json: birds, except: [:created_at, :updated_at] # to filter
+    render json: birds.to_json(except: [:created_at, :updated_at])
   end
   def show
    bird = Bird.find_by(id: params[:id])
